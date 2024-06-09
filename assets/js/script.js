43d40e7b05afdea12550d64c79428d79b7c55c3a
Cássio Machado <cassiomachado1995@outlook.com>
2024-06-09 12:56:59 -0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 
+// area onde vai preencher as informações
+let areas = {
+    a: null,
+    b: null,
+    c: null
+}
+
+
+
 document.querySelectorAll('.item').forEach(item => {
                 // Evento do começo do arrasta
     item.addEventListener('dragstart', dragStart);
@@ -15,6 +24,10 @@ document.querySelectorAll('.area').forEach( area =>{
 
 
 
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral)
+
 // Functions Item
 function dragStart(e) {
     e.currentTarget.classList.add('dragging');
@@ -28,10 +41,13 @@ function dragEnd(e){
 
 
 // Functions Area
-function dragOver (e) {
+function dragOver (e) {// só funciona quando nao tem nenhum item dentro onde quer soltar
+    if (e.currentTarget.querySelector('.item') === null){
+
     e.preventDefault();
             // adicionar  a classe hover 
     e.currentTarget.classList.add('hover');
+    }
 };
 
 function dragLeave (e){
@@ -44,11 +60,63 @@ function drop (e) {
     // quando der o drop remover o hover
     e.currentTarget.classList.remove('hover');
 
-    let dragItem = document.querySelector('.item.dragging');
 
 
     if (e.currentTarget.querySelector('.item') === null){
             // appendChild para adicionar um elemento dentro do item
+            let dragItem = document.querySelector('.item.dragging');
+
         e.currentTarget.appendChild(dragItem);
+        updateAreas();
     }
+}
+
+
+// functions neutral area
+
+
+function dragOverNeutral (e) {
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+};
+
+function dragLeaveNeutral (e) {
+    e.currentTarget.classList.remove('hover')
+};
+
+
+function dropNeutral (e) {
+    e.currentTarget.classList.remove('hover');
+
+    // Processo de drop 
+    let dragItem = document.querySelector('.item.dragging');// aqui ele pega o item
+
+        e.currentTarget.appendChild(dragItem);// aqui ele adiona no neutralarea
+        updateAreas();
+};
+
+
+// Logic Functions
+
+function updateAreas () {
+    document.querySelectorAll('.area').forEach(area => {
+        // pegar o noem da area
+        let name = area.getAttribute('data-name');
+
+        if (area.querySelector('.item') !== null){
+            areas[name] = area.querySelector('.item').innerHTML;
+
+        }else {
+            areas [name] = null;
+        }
+    });
+    // verificação 
+    if (areas.a === '1' && areas.b === '2' && areas.c === '3' ){
+        // acontecendo isso trocamos a classe abaixo
+        document.querySelector('.areas').classList.add('correct');
+    } else {
+        document.querySelector('.areas').classList.remove('correct');
+
+    }
+
 }
